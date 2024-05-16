27262d47bfaedd6f66ab29fbd6d377d65b75a984
Adora <adoraonyile@gmail.com>
2024-05-16 23:20:38 +0100

    debug 02

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,38 +3,23 @@
 Fabric script that distributes an archive to your web servers
 """
 
-from datetime import datetime
 from fabric.api import *
-import os
+import os.path import exists
 
 env.hosts = ['54.234.97.54', '100.25.190.116']
 env.user = "ubuntu"
-
-
-def do_pack():
-    """
-        return the archive path if archive has generated correctly.
-    """
-
-    local("mkdir -p versions")
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archived_f_path = "versions/web_static_{}.tgz".format(date)
-    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
-
-    if t_gzip_archive.succeeded:
-        return archived_f_path
-    else:
-        return None
 
 
 def do_deploy(archive_path):
     """
         Distribute archive.
     """
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(newest_version))
         run("sudo tar -xzf {} -C {}/".format(archived_file,
@@ -45,8 +30,6 @@ def do_deploy(archive_path):
         run("sudo rm -rf {}/web_static".format(newest_version))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -s {} /data/web_static/current".format(newest_version))
-
-        print("New version deployed!")
         return True
-
-    return False
+    except:
+        return False
